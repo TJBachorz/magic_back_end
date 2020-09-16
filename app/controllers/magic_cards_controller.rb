c36b37f93753f6c5bf588f8ae20f599e9f4dd77d
TJBachorz <TJBachorz@gmail.com>
2020-09-16 17:36:46 -0600

    updates resources and controllers

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -4,4 +4,9 @@ class MagicCardsController < ApplicationController
         @magicCards = MagicCard.all
         render json: @magicCards
     end
+
+    def destroy
+        @magicCard = MagicCard.find(params[:id])
+        @magicCard.destroy
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :likes, only: [:index]
-  resources :magic_cards, only: [:index]
+  resources :magic_cards, only: [:index, :destroy]
   resources :users, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
